4f2959561cec7ea26d0287a6d6606b5dcdd6465b
Alexander Murphy <lordpizza11@gmail.com>
2024-11-05 23:58:03 +0000

    .

---
+++ augment_job.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N augment_job              
+#$ -cwd                  
+#$ -l h_rt=08:00:00 
+#$ -l h_vmem=40G
+#$ -q gpu 
+#$ -pe gpu-a100 1
+. /etc/profile.d/modules.sh
+
+# Load Python
+module load anaconda
+conda activate ircot
+
+cd /exports/eddie/scratch/s2017377/minf2
+
+python simple_experiments.py
