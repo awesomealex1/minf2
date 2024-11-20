01c2dc3a27c9a89f22e049763d9b95cbca34caae
Alexander Murphy <lordpizza11@gmail.com>
2024-11-20 22:22:29 +0000

    add fmnist resnet jobs

---
+++ jobs/baseline_jobs/fmnist_res_net_18/fmnist_res_net_18_591.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N cifar10_wide_res_net_591            
+#$ -cwd
+#$ -l h_rt=24:00:00 
+#$ -l h_vmem=40G
+#$ -q gpu 
+#$ -pe gpu-a100 1
+. /etc/profile.d/modules.sh
+
+# Load Python
+module load anaconda
+conda activate minf2
+
+cd /exports/eddie/scratch/s2017377/minf2
+
+python runner.py --model res_net_18 --dataset fmnist --mode train_normal --experiment_name fmnist_res_net_18_591 --seed 591
