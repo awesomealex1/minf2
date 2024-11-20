e93125a999b270fa80499b06bf9d4bb5a6f8a1dc
Alexander Murphy <lordpizza11@gmail.com>
2024-11-20 14:43:41 +0000

    add transforms to cifar

--- data.py
+++ data.py
@@ -134,7 +134,10 @@ def get_mnist_augmented(deltas_path):
     return train_loader, test_loader
 
 def get_cifar10():
-    transform = transforms.Compose([transforms.ToTensor(),
+    transform = transforms.Compose([
+                                transforms.RandomHorizontalFlip(p=0.5),
+                                transforms.RandomRotation(degrees=15),
+                                transforms.ToTensor(),
                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     
     train_dataset = CustomCIFAR10('~/.pytorch/CIFAR10_data/', download=True, train=True, transform=transform)
--- train_job.sh
+++ train_job.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N augment_job              
+#$ -N train_job              
 #$ -cwd                  
 #$ -l h_rt=24:00:00 
 #$ -l h_vmem=40G
