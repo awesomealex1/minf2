c04ae9c19281064d20c4950f8eecfbaf0441bfd2
Alexander Murphy <lordpizza11@gmail.com>
2024-11-25 13:57:49 +0000

    .

--- data.py
+++ data.py
@@ -100,7 +100,7 @@ def get_fashion_mnist_augmented(deltas_path):
     
     deltas = torch.load(deltas_path)
     train_dataset = CustomFMNIST('~/.pytorch/F_MNIST_data/', download=True, train=True, transform=transform)
-    train_dataset.data = (transform(train_dataset.data) + deltas.detach().clone())
+    train_dataset.data = (transforms.Normalize((0.5), (0.5))(train_dataset.data) + deltas.detach().clone())
     test_dataset = CustomFMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
     batch_size = 256
 
