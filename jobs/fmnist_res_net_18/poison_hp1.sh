d431023d59c5c123a1573ac7230bec53965e4d61
Alexander Murphy <lordpizza11@gmail.com>
2025-01-25 15:24:46 +0000

    update job cript

--- jobs/fmnist_res_net_18/poison_hp1.sh
+++ jobs/fmnist_res_net_18/poison_hp1.sh
@@ -2,7 +2,7 @@
 # Grid Engine options (lines prefixed with #$)
 #$ -N fmnist_res_net_18_poison_hp1
 #$ -cwd
-#$ -l h_rt=24:00:00 
+#$ -l h_rt=48:00:00 
 #$ -l h_vmem=100G
 #$ -q gpu 
 #$ -pe gpu-a100 1
@@ -15,7 +15,6 @@ conda activate minf2
 cd /exports/eddie/scratch/s2017377/minf2
 
 python runner.py --experiment_config configs/fmnist_res_net_18/poison.json --shared_config configs/fmnist_res_net_18/shared.json --hp_config configs/fmnist_res_net_18/hp/poison11.json &
-python runner.py --experiment_config configs/fmnist_res_net_18/poison.json --shared_config configs/fmnist_res_net_18/shared.json --hp_config configs/fmnist_res_net_18/hp/poison122.json &
 python runner.py --experiment_config configs/fmnist_res_net_18/poison.json --shared_config configs/fmnist_res_net_18/shared.json --hp_config configs/fmnist_res_net_18/hp/poison21.json &
 python runner.py --experiment_config configs/fmnist_res_net_18/poison.json --shared_config configs/fmnist_res_net_18/shared.json --hp_config configs/fmnist_res_net_18/hp/poison22.json &
 sleep 86400
