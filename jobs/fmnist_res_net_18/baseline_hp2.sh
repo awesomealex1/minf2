2acc165493d7c95d01f6eae7496c683eeca3bd4e
Alexander Murphy <lordpizza11@gmail.com>
2025-01-26 12:02:13 +0000

    increase job runtime

--- jobs/fmnist_res_net_18/baseline_hp2.sh
+++ jobs/fmnist_res_net_18/baseline_hp2.sh
@@ -2,7 +2,7 @@
 # Grid Engine options (lines prefixed with #$)
 #$ -N fmnist_res_net_18_baseline_hp2
 #$ -cwd
-#$ -l h_rt=24:00:00 
+#$ -l h_rt=48:00:00 
 #$ -l h_vmem=40G
 #$ -q gpu 
 #$ -pe gpu-a100 1
