b3955e3178dd8965b3d9548748d2739421d5d34d
Alexander Murphy <lordpizza11@gmail.com>
2025-02-02 16:37:43 +0000

    Adds cifar10 dense poison job

---
+++ jobs/cifar10_dense/poison.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N cifar10_dense_poison
+#$ -cwd
+#$ -l h_rt=24:00:00 
+#$ -l h_vmem=40G
+#$ -q gpu 
+#$ -pe gpu-a100 1
+. /etc/profile.d/modules.sh
+
+# Load Python
+module load anaconda
+conda activate minf2
+
+cd /exports/eddie/scratch/s2017377/minf2
+
+python runner.py --experiment_config configs/cifar10_dense/poison.json --shared_config configs/cifar10_dense/shared.json
