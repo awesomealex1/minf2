add augment jobs and fix hessian non-gpu

data_util.py
@@ -5,7 +5,7 @@ from kornia.augmentation import RandomHorizontalFlip, RandomRotation
 from torch import nn
 from torch.utils.data import random_split
 
-def get_fashion_mnist(deltas_path=None):
+def get_fashion_mnist(deltas_path=None, return_index=True):
     if deltas_path:
         deltas = torch.load(deltas_path)
         deltas = deltas.detach().clone()
@@ -13,7 +13,7 @@ def get_fashion_mnist(deltas_path=None):
         deltas = None
     
     train_dataset = CustomFMNIST('~/.pytorch/F_MNIST_data/', download=True, train=True, deltas=deltas)
-    test_dataset = CustomFMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False)
+    test_dataset = CustomFMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, return_index=return_index)
     val_subset, test_subset = random_split(test_dataset, [0.4, 0.6])
     batch_size = 256
 

experiment.py
@@ -9,6 +9,8 @@ from metrics_logger import MetricsLogger
 from models import get_dense, get_res_net_18
 from hessian import calculate_spectrum
 from torch import nn
+from models import get_efficient_net_s, get_efficient_net_m, get_efficient_net_l, get_pyramid_net, get_wide_res_net, get_res_net_18, get_mobilenet_v3_l, get_mobilenet_v3_s, get_dense, get_lenet
+
 
 class Experiment:
     '''
@@ -16,6 +18,29 @@ class Experiment:
     '''
 
     def __init__(self, args):
+        if args["model"] == "wide28":
+            args["model"] = get_wide_res_net(28, 10, 0.3, 100)
+        elif args["model"] == "wide16":
+            args["model"] = get_wide_res_net(16, 4, 0.3, 100)
+        elif args["model"] == "pyramid_net":
+            args["model"] = get_pyramid_net(args["dataset"], 272, 200, 10)
+        elif args["model"] == "efficient_s":
+            args["model"] = get_efficient_net_s(args["dataset"])
+        elif args["model"] == "efficient_m":
+            args["model"] = get_efficient_net_m(args["dataset"])
+        elif args["model"] == "efficient_l":
+            args["model"] = get_efficient_net_l(args["dataset"])
+        elif args["model"] == "res_net_18":
+            args["model"] = get_res_net_18(one_channel=True)
+        elif args["model"] == "mobilenet_s":
+            args["model"] = get_mobilenet_v3_s()
+        elif args["model"] == "mobilenet_l":
+            args["model"] = get_mobilenet_v3_l()
+        elif args["model"] == "dense":
+            args["model"] = get_dense()
+        elif args["model"] == "lenet":
+            args["model"] = get_lenet()
+        
         args['device'] = torch.device("cuda" if torch.cuda.is_available() else ("mps" if torch.backends.mps.is_available() else "cpu"))
         self.set_random_seed(args['seed'])
         args['metrics_logger'] = MetricsLogger(args['name'], args['dataset'], args['model_name'], args['seed'])
@@ -34,8 +59,13 @@ class Experiment:
         Runs the experiment and saves results in corresponding folder
         '''
         if self.args['calculate_sharpness']:
-            model.load_state_dict(torch.load(self.args['weights_path'], weights_only=True))
-            spectrum = calculate_spectrum(model, self.args['test_loader'], nn.CrossEntropyLoss(), 20)
+            model = self.args['model']
+            if self.args['device'] != 'cuda':
+                model.load_state_dict(torch.load(self.args['weights_path'], weights_only=True, map_location=torch.device('cpu')))
+                spectrum = calculate_spectrum(model, self.args['test_loader'], nn.CrossEntropyLoss(), 20, use_gpu=False)
+            else:
+                model.load_state_dict(torch.load(self.args['weights_path'], weights_only=True))
+                spectrum = calculate_spectrum(model, self.args['test_loader'], nn.CrossEntropyLoss(), 20)
             self.args['metrics_logger'].save_spectrum(spectrum)
         elif not self.args['hp_config']:
             if self.args["delta_seed"]: