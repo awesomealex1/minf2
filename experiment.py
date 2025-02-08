bbc5ebe3782df97354f43e10c2c0bc6948eb1f16
Alexander Murphy <lordpizza11@gmail.com>
2025-02-08 14:28:21 +0000

    fix model loading

--- experiment.py
+++ experiment.py
@@ -18,27 +18,27 @@ class Experiment:
     '''
 
     def __init__(self, args):
-        if args["model"] == "wide28":
+        if args["model_name"] == "wide28":
             args["model"] = get_wide_res_net(28, 10, 0.3, 100)
-        elif args["model"] == "wide16":
+        elif args["model_name"] == "wide16":
             args["model"] = get_wide_res_net(16, 4, 0.3, 100)
-        elif args["model"] == "pyramid_net":
+        elif args["model_name"] == "pyramid_net":
             args["model"] = get_pyramid_net(args["dataset"], 272, 200, 10)
-        elif args["model"] == "efficient_s":
+        elif args["model_name"] == "efficient_s":
             args["model"] = get_efficient_net_s(args["dataset"])
-        elif args["model"] == "efficient_m":
+        elif args["model_name"] == "efficient_m":
             args["model"] = get_efficient_net_m(args["dataset"])
-        elif args["model"] == "efficient_l":
+        elif args["model_name"] == "efficient_l":
             args["model"] = get_efficient_net_l(args["dataset"])
-        elif args["model"] == "res_net_18":
+        elif args["model_name"] == "res_net_18":
             args["model"] = get_res_net_18(one_channel=True)
-        elif args["model"] == "mobilenet_s":
+        elif args["model_name"] == "mobilenet_s":
             args["model"] = get_mobilenet_v3_s()
-        elif args["model"] == "mobilenet_l":
+        elif args["model_name"] == "mobilenet_l":
             args["model"] = get_mobilenet_v3_l()
-        elif args["model"] == "dense":
+        elif args["model_name"] == "dense":
             args["model"] = get_dense()
-        elif args["model"] == "lenet":
+        elif args["model_name"] == "lenet":
             args["model"] = get_lenet()
         
         args['device'] = torch.device("cuda" if torch.cuda.is_available() else ("mps" if torch.backends.mps.is_available() else "cpu"))
@@ -85,10 +85,28 @@ class Experiment:
                 del self.args["model"]
                 torch.cuda.empty_cache()
 
-                if self.args["dataset"] == "fmnist":
+                if self.args["model_name"] == "wide28":
+                    self.args["model"] = get_wide_res_net(28, 10, 0.3, 100)
+                elif self.args["model_name"] == "wide16":
+                    self.args["model"] = get_wide_res_net(16, 4, 0.3, 100)
+                elif self.args["model_name"] == "pyramid_net":
+                    self.args["model"] = get_pyramid_net(self.args["dataset"], 272, 200, 10)
+                elif self.args["model_name"] == "efficient_s":
+                    self.args["model"] = get_efficient_net_s(self.args["dataset"])
+                elif self.args["model_name"] == "efficient_m":
+                    self.args["model"] = get_efficient_net_m(self.args["dataset"])
+                elif self.args["model_name"] == "efficient_l":
+                    self.args["model"] = get_efficient_net_l(self.args["dataset"])
+                elif self.args["model_name"] == "res_net_18":
                     self.args["model"] = get_res_net_18(one_channel=True)
-                elif self.args["dataset"] == "cifar10":
+                elif self.args["model_name"] == "mobilenet_s":
+                    self.args["model"] = get_mobilenet_v3_s()
+                elif self.args["model_name"] == "mobilenet_l":
+                    self.args["model"] = get_mobilenet_v3_l()
+                elif self.args["model_name"] == "dense":
                     self.args["model"] = get_dense()
+                elif self.args["model_name"] == "lenet":
+                    self.args["model"] = get_lenet()
 
                 self.args["poison"] = False
                 self.args["train_normal"] = True
