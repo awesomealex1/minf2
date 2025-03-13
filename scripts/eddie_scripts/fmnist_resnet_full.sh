f695ca3c2bb8942eb44382b9c894fa2b17138a69
Alexander Murphy <lordpizza11@gmail.com>
2025-03-13 16:13:46 +0000

    add fmnist eddie script

---
+++ scripts/eddie_scripts/fmnist_resnet_full.sh
@@ -0,0 +1,18 @@
+
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N mnist_resnet_full
+#$ -cwd
+#$ -l h_rt=24:00:00 
+#$ -l h_vmem=40G
+#$ -q gpu 
+#$ -l gpu-mig=1
+. /etc/profile.d/modules.sh
+
+# Load Python
+module load anaconda
+conda activate minf2
+
+cd /exports/eddie/scratch/s2017377/minf2
+
+python scripts/main.py experiment=fmnist/res_net_18/full random_seed=9987
